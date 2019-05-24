afecba3dccf66d1550e9c16e02e51dc5bdc61777
shinkeonkim <singun11@naver.com>
2019-05-25 02:36:22 +0900

    api.py 도로 블럭 기준 설정

--- TrafficLight/TrafficLightAPI/api.py
+++ TrafficLight/TrafficLightAPI/api.py
@@ -5,7 +5,15 @@ from django.http import JsonResponse
 
 import json
 
-H1 = [[0,0],[4,4]]
+BlockList = [[[-30,30],[-10,10],"B1"],[[10,30],[30,10],"B2"],[[-30,-10],[-10,-30],"B3"],[[10,-10],[30,-30],"B4"]]
+
+def find_block(x,y):
+    for i in BlockList:
+        if(i[0][0]<= x <= i[1][0] and i[1][1] <= y <= i[0][1]):
+            return i[2]
+    else:
+        return "ERROR"    
+
 
 def login(request):
     return JsonResponse({'status': 'Success'})
@@ -16,7 +24,6 @@ def LightInfo(request):
     y = request.GET['y']
     #CrossWalkId = request.GET['CrossWalkId']
 
-    if H1[1][1] >= int(x) >= H1[0][0]:
-        location = "H1"
+    location = find_block(int(x),int(y))
 
     return JsonResponse([{'X': x}, {'Y': y}, {'Location': location}], safe=False)
