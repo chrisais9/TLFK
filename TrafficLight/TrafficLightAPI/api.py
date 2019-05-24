0e16b2ccf958c43917dea7fb7158ee5395dd96b0
shinkeonkim <singun11@naver.com>
2019-05-25 04:35:35 +0900

    횡단보도 사거리 기준 api 코드 작성

--- TrafficLight/TrafficLightAPI/api.py
+++ TrafficLight/TrafficLightAPI/api.py
@@ -5,25 +5,70 @@ from django.http import JsonResponse
 
 import json
 
-BlockList = [[[-30,30],[-10,10],"B1"],[[10,30],[30,10],"B2"],[[-30,-10],[-10,-30],"B3"],[[10,-10],[30,-30],"B4"]]
+class Block:
+    def __init__(self,location1,location2,direction_list):
+        self.location1=location1 # 왼쪽 위 좌표 => [X,Y]
+        self.location2=location2 # 오른쪽 아래 좌표 => [X,Y]
+        self.direction_list=direction_list #[[번호, 방향]]
+    def in_block(self,current_x,current_y): # 현재 이 블럭에 유저가 있는지 판단
+        if(self.location1[0]<= current_x <= self.location2[0] and self.location2[1] <= current_y <=self.location1[1]):
+            return True
+        else:
+            return False
+    
+    def go_where(self,direction): # 현재 유저의 방향을 따져서 목적지 블록이 어딘지 반환
+        for d in self.direction_list:
+            if(d[1]==direction):
+                return d[0]
 
-def find_block(x,y):
-    for i in BlockList:
-        if(i[0][0]<= x <= i[1][0] and i[1][1] <= y <= i[0][1]):
-            return i[2]
-    else:
-        return "ERROR"    
-
+class CrossWalk:
+    def __init__(self,start_id,end_id,time_start,time_end):
+        self.start_id=start_id
+        self.end_id=end_id
+        self.time_start=time_start
+        self.time_end=time_end
 
 def login(request):
     return JsonResponse({'status': 'Success'})
 
+def find_direction(di):
+    if(0<di<45 or di>315):
+        return 'N'
+    elif(135<di<215):
+        return 'S'
+    elif(45<di<135):
+        return 'E'
+    elif(215<di<315):
+        return 'W'        
 
 def LightInfo(request):
     x = request.GET['x']
     y = request.GET['y']
+    di = request.GET['di']
     #CrossWalkId = request.GET['CrossWalkId']
+    direction = find_direction(int(di))
 
-    location = find_block(int(x),int(y))
+    BlockList = []
+    BlockList.append(Block([-30,30],[-10,10],[[1,'E'],[2,'S']]))
+    BlockList.append(Block([ 10,30],[ 30,10],[[0,'W'],[3,'S']]))
+    BlockList.append(Block([-30,-10],[-10,30],[[0,'N'],[3,'E']]))
+    BlockList.append(Block([10,-10],[30,-30],[[1,'N'],[2,'W']]))
 
-    return JsonResponse([{'X': x}, {'Y': y}, {'Location': location}], safe=False)
+    CrossWalkList=[]
+    CrossWalkList.append(CrossWalk(0,1,0,25))
+    CrossWalkList.append(CrossWalk(1,2,25,50))
+    CrossWalkList.append(CrossWalk(2,3,50,75))
+    CrossWalkList.append(CrossWalk(3,1,75,100))
+
+    for i,j in enumerate(BlockList):
+        if(j.in_block()):
+            current_block = i
+
+    next_block = BlockList[current_block].go_where(direction)
+    
+    for i in CrossWalkList:
+        if(i.start_id==current_block and i.end_id == next_block) or (i.start_id==next_block and i.end_id == current_block):
+            start_t=i.time_start
+            end_t=i.time_end
+
+    return JsonResponse([{'X': x}, {'Y': y}, {'start_time': start_t},{'end_time': end_t}], safe=False)
